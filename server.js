a3383c27fa3cd049fd3c9cd2b5cd8e54cfa2f102
Michael Robards <mikerobards@icloud.com>
2022-07-18 14:31:52 -0400

    add layout hbs

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const friendsRouter = require('./routes/friends.router');
 const messagesRouter = require('./routes/messages.router');
 
 const app = express();
-const path = require('path');
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 const PORT = 3000;
 
 app.use((req, res, next) => {
     const start = Date.now();
     next();
-    // actions go here...
     const delta = Date.now() - start;
     console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
@@ -19,10 +21,14 @@ app.use((req, res, next) => {
 app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-app.use('./friends', friendsRouter);
-// mounting the friendsRouter on the app object 
-// need to call app.use so that express knows to use this path for the router
-app.use('./messages', messagesRouter);
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My Friends Are VERYY Clever',
+        caption: 'Let\'s go skiing!',
+    });
+});
+app.use('/friends', friendsRouter);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`);
